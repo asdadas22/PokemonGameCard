482de8af4e16dd246416c8e70a1bf7564c1b0c23
asdadas22 <suarez_pro@hotmail.com>
2023-10-08 13:23:23 -0300

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCreateComponent } from './src/components/pokemon-create/pokemon-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/src/components/pokemon-create/pokemon-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-create',
+  templateUrl: './pokemon-create.component.html',
+  styleUrls: ['./pokemon-create.component.css']
+})
+export class PokemonCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
